2f08fd7a1bf1f8320ec856ad132657b26f0c670f
SergiooOC <118816651+SergiooOC@users.noreply.github.com>
2023-01-10 01:07:44 +0100

    Update GitClient.java

--- GitClient.java
+++ GitClient.java
@@ -50,11 +50,5 @@ public class GitClient {
     public void setLicencia(String licencia) {
         this.licencia = licencia;
     }   
-
-    @Override
-    public String toString() {
-        return "GitClient{" + "cliente=" + cliente + ", version=" + version + ", sistemOper=" + sistemOper + ", licencia=" + licencia + '}';
-    }
-    
     
 }
